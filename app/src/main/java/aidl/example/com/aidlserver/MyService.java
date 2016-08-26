01ee69c5b28562cc775453421d0ca400a009eb68
root0301 <2648398526@qq.com>
2016-08-26 11:05:00 +0800

    server

--- app/src/main/java/aidl/example/com/aidlserver/MyService.java
+++ app/src/main/java/aidl/example/com/aidlserver/MyService.java
@@ -10,7 +10,9 @@ import com.example.aidl.IMyAidlInterface;
 
 public class MyService extends Service {
 
-    public class MyServiceImpl extends IMyAidlInterface.Stub {
+    private String TAG = "AIDL";
+
+/*    public class MyServiceImpl extends IMyAidlInterface.Stub {
         @Override
         public void basicTypes(int anInt, long aLong, boolean aBoolean, float aFloat,
                                double aDouble, String aString) throws RemoteException {
@@ -21,11 +23,38 @@ public class MyService extends Service {
             Log.d("aidl", "helloAndroidAIDL heard from : " + name);
             return "Rust: Service01 return value successfully!";
         }
-    }
+    }*/
+
+    IMyAidlInterface.Stub stub = new IMyAidlInterface.Stub() {
+
+        @Override
+        public void basicTypes(int anInt, long aLong, boolean aBoolean, float aFloat, double aDouble, String aString) throws RemoteException {
+        }
+
+        @Override
+        public String helloAndroidAIDL(String name) throws RemoteException {
+            Log.d(TAG, "helloAndroidAIDL call from: " + name);
+            return "Rust: Service01 return value successfully!";
+        }
+    };
+
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return new MyServiceImpl();// 返回内部类实例
+        Log.d(TAG,"onBind call");
+        return stub;// 返回内部类实例
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.d(TAG,"onUnbind call");
+        return true;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy call");
     }
 }
